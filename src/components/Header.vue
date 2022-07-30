2aa0b03a66b25e3516ad7856e4e9bce436f6f26c
Anonym <anonym1120@gmail.com>
2022-07-31 03:15:12 +0800

    tidy up

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,10 @@
+<template>
+<div></div>
+</template>
+
 <script setup>
 
 </script>
 
-<template>
-    <div>
-        
-    </div>
-</template>
+<style scoped>
+</style>
--- src/components/Tags.vue
+++ src/components/Tags.vue
@@ -1,10 +1,10 @@
-<script setup>
-
-</script>
-
 <template>
 <div></div>
 </template>
+
+<script setup>
+
+</script>
 
 <style scoped>
 </style>
