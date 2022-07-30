5c08f6776087b0dd2dad7ec85f0d939879116a68
Anonym <anonym1120@gmail.com>
2022-07-30 20:30:20 +0800

    fix element.js

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,5 +3,5 @@ import localeZH from 'element-plus/lib/locale/lang/zh-tw'
 import 'element-plus/theme-chalk/index.css'
 
 export default (app) => {
-    app.use(elementPlus, {locale: localeZH})
+    app.use(elementPlus, { locale: localeZH })
 }
