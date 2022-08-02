f7d3c2829dc97b297d8ce03934468b8c399800da
Anonym <anonym1120@gmail.com>
2022-08-02 22:17:08 +0800

    add sidebarData

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,10 +1,21 @@
 <template>
-<div></div>
+    <div class="sidebar">
+        <el-menu class="sidebar-el-menu">
+
+        </el-menu>
+    </div>
 </template>
 
+
+
 <script setup>
+import { computed } from "vue";
+import { useSidebarStore } from '../store/sidebar'
+import { useRoute } from "vue-router";
 
 </script>
+
+
 
 <style scoped>
 </style>
